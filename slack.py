7fe609b94c8d6097fc6d98b9d1aa20e773d55c40
ningaz <ningaz@mail.ru>
2016-12-28 12:01:25 +0300

    Update slack.py

--- slack.py
+++ slack.py
@@ -2,20 +2,20 @@ from slacker import Slacker
 
 class Notifier():
     msgQueue = []
-    slack = Slacker('xoxb-98814092768-gDZCppKCakyAZpio3KYw72Tx')
+    slack = Slacker('put this your Slack token')
     def getNotification(self):
         #print len(self.msgQueue)
         if  len(self.msgQueue) > 1:
             return self.msgQueue.pop(0)
         return ''
     def sendSlackNotification(self, msg, file = None):
-        #self.slack.chat.post_message('#vaapi', msg, '@vabot',icon_emoji='vhs',as_user=True)
+        #self.slack.chat.post_message('#channel', msg, '@botname',icon_emoji='icon',as_user=True)
         if(file != None):     
-		self.slack.files.upload(file_=file,filetype='jpeg',title='New guest',channels='#vaapi')
+		self.slack.files.upload(file_=file,filetype='jpeg',title='New guest',channels='#channel')
 
     def setNotification(self, msg):
         self.msgQueue.append(msg)
         #print msg
 		
 notify = Notifier()		
-notify.sendSlackNotification(str('New guest'),'/home/root/image.jpeg')
+notify.sendSlackNotification(str('New guest'),'path to image.jpeg')
